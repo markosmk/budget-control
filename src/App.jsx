584dfc6b7282e9bc739ca28407d81c906bbfc05a
MarkosMk <markosmk@me.com>
2022-12-03 11:47:40 -0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="container mx-auto my-40">
+      <h3 className="text-xl text-cyan-400 text-center">Vite - React - TailwindCss - Eslint</h3>
+    </main>
+  );
+}
+
+export default App;
