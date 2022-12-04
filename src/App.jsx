3bb2bcf7c4f47eca3f01f6756b37c87d11ce3542
MarkosMk <markosmk@me.com>
2022-12-03 23:46:36 -0300

    feat: added tailwind css

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <main className="container mx-auto my-40">
-      <h3 className="text-xl text-cyan-400 text-center">Vite - React - TailwindCss - Eslint</h3>
-    </main>
-  );
-}
-
-export default App;
