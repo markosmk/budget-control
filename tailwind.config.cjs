2511dba7a8d59dda1fd24c5c9691a5ed0f7700be
MarkosMk <markosmk@me.com>
2022-12-05 00:02:18 -0300

    chore: added new color tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,22 @@ module.exports = {
   darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cool: {
+          50: '#E5F1FF',
+          100: '#CCE4FF',
+          200: '#99C9FF',
+          300: '#66ADFF',
+          400: '#3392FF',
+          500: '#0076FF',
+          600: '#005FCC',
+          700: '#004799',
+          800: '#003066',
+          900: '#001833',
+        },
+      },
+    },
   },
   plugins: [],
 };
